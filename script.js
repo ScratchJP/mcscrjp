4c7ee20381890a1544f19680f0da8b39d09ed347
Driplase <258vr.g.subspeed@gmail.com>
2024-07-25 12:27:44 +0900

    ready for, uh...

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ function rd(_link) {
     let d = .3;
     let e = 0;
     const g = new Date().getTime();
+    let l = false;
     const ca = setInterval(function(){
         document.body.style.animation = undefined;
         document.body.style.opacity = 1 - easeOutQuart(e / 100);
         e = (new Date().getTime() - g) / 10 / d;
         e = Math.round(e * 100) / 100;
-        if (e > 80) {
-            clearInterval(ca);
+        if (e > 80 && l === false) {
             location.href = _link;
             document.body.style.animation = "begin 1.6s cubic-bezier(0.25, 1, 0.5, 1);";
+            l = true;
+        }
+        if (e > 100) {
+            clearInterval(ca);
+            document.body.style.opacity = 1;
         }
     }, 1);
 }
