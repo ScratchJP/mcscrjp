fa9222cece1af14ecc6cfcebb750bad1dac89e4a
Driplase <258vr.g.subspeed@gmail.com>
2025-02-01 19:55:31 +0900

    added images for night! pretty useless though...

--- script.js
+++ script.js
@@ -42,3 +42,11 @@ As.forEach(i => {
         }
     })
 })
+const date = new Date(), ni = document.querySelectorAll('img[nightsrc]');
+ni.forEach(i => {
+    // 18 ~ 30  -7
+    const T = date.getHours() - 7;
+    if (T >= 11 && T <= 23) {
+        i.src = i.attributes.nightsrc.value
+    }
+})
