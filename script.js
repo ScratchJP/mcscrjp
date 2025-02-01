af42ec0b091e981b22b227335b84d39e20b3564d
Driplase <258vr.g.subspeed@gmail.com>
2025-02-01 16:34:44 +0900

    page in-betweens improvation

--- script.js
+++ script.js
@@ -24,9 +24,21 @@ const As = document.querySelectorAll('a:not([target])');
 As.forEach(i => {
     i.addEventListener("click", e => {
         if (e.ctrlKey||e.metaKey||e.altKey||e.shiftKey) return;
-        e.preventDefault();
-        console.log(e.target.href)
-        document.body.style.opacity = 0;
-        setTimeout(() => location.href = e.target.href, 300)
+        try {
+            const tu = new URL(e.target.href)
+            if (tu.host !== location.host) {
+                e.preventDefault()
+                window.open(e.target.href, "_blank").focus()
+                return;
+            }
+            if (tu.pathname === location.pathname) return;
+            e.preventDefault();
+            const dbs = document.body.style;
+            dbs.opacity = 0;
+            setTimeout(() => location.href = e.target.href, 300)
+        } catch (er) {
+            console.error(er)
+            location.href = e.target.href
+        }
     })
 })
