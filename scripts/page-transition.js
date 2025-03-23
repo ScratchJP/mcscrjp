2ef73483d73323f3a33247347be6945af8a5ac81
Driplase <258vr.g.subspeed@gmail.com>
2025-03-23 11:48:04 +0900

    alright this is probably good to go

--- scripts/page-transition.js
+++ scripts/page-transition.js
@@ -24,7 +24,6 @@ As.forEach(i => {
 
 window.addEventListener('pageshow', e => {
     if (["reload", 'back_forward'].includes(performance.getEntriesByType("navigation")[0].type)) {
-        console.log('welcome back...')
         try { clearInterval(inter) } catch (er) { console.error(er) }
         document.body.style.opacity = 1;
     }
