65bcb96a819015cf8c3e7b2f73f9ac4bbdbb72c6
Driplase <258vr.g.subspeed@gmail.com>
2025-03-24 22:34:05 +0900

    hmmmmmm

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: ".",
-      output: "docs",
+      output: "_site",
       includes: "_includes",
       layouts: "_layouts"
     }
